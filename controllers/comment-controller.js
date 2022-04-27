afdeaab6424ee9d43ffea142889e56994e3cd5f5
Carolyn B <bmathematics@gmail.com>
2022-04-27 14:12:11 -0400

    connect front end to back end api

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,7 +1,7 @@
 const { Comment, Pizza } = require("../models");
 
 const commentController = {
-  //add comment to pizza
+  // add comment to pizza
   addComment({ params, body }, res) {
     console.log(body);
     Comment.create(body)
@@ -12,7 +12,7 @@ const commentController = {
           { new: true }
         );
       })
-      .then((dbPizzaData => {
+      .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
           return;
@@ -22,28 +22,28 @@ const commentController = {
       .catch((err) => res.json(err));
   },
 
-  //remove comment
-  removeComment({ params }, res){
-      Comment. findOneAndDelete({ _id: params.commentId })
-      .then(deleteComment => {
-          if(!deleteComment){
-              return res.status(404).json({ message: 'No comment with this id!' });
-          }
-          return Pizza.findOneAndUpdate(
-              { _id: params.pizzaId },
-              { $pull: { comments: params.commentId } },
-              { new: true }
-          );
+  // remove comment
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+      .then((deletedComment) => {
+        if (!deletedComment) {
+          return res.status(404).json({ message: "No comment with this id!" });
+        }
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $pull: { comments: params.commentId } },
+          { new: true }
+        );
       })
-      .then(dbPizzaData => {
-          if (!dbPizzaData) {
-              res.status(404).json({ message: 'No pizza sound with this id!' });
-              return;
-          }
-          res.json(dbPizzaData);
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
       })
-      .catch(err => res.json(err));
-  }
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = commentController;
